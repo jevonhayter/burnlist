c2104433e47f8c4530c2eac91d4d85f66386252e
jevon hayter <jevonhayter@gmail.com>
2018-10-10 13:26:59 +0000

    added alert system

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -30,7 +30,7 @@ class ListsController < ApplicationController
 
     respond_to do |format|
       if @list.save
-        format.html { redirect_to @list, notice: 'Item was successfully created.' }
+        format.html { redirect_to lists_url, alert: 'alert-success', notice: 'Item was successfully created.' }
         format.json { render :show, status: :created, location: @list }
       else
         format.html { render :new }
@@ -44,7 +44,7 @@ class ListsController < ApplicationController
   def update
     respond_to do |format|
       if @list.update(list_params)
-        format.html { redirect_to @list, notice: 'List was successfully updated.' }
+        format.html { redirect_to @list, alert: 'alert-warning', notice: 'List was successfully updated.' }
         format.json { render :show, status: :ok, location: @list }
       else
         format.html { render :edit }
@@ -58,7 +58,7 @@ class ListsController < ApplicationController
   def destroy
     @list.destroy
     respond_to do |format|
-      format.html { redirect_to lists_url, notice: 'List was successfully destroyed.' }
+      format.html { redirect_to lists_url, alert: 'alert-danger', notice: 'List was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
